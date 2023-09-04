08f508552cd51930eaa5c0264e04463c4783ae40
Chako <ryland0000@gmail.com>
2023-09-04 01:26:45 -0700

    DIVISION and TEAMS tables

--- exercise1.sql
+++ exercise1.sql
@@ -4,3 +4,17 @@ CREATE TABLE cars (
 	make TEXT NOT NULL,
 	model TEXT NOT NULL
 );
+
+CREATE TABLE divisions (
+	id SERIAL PRIMARY KEY,
+	name TEXT NOT NULL UNIQUE
+);
+
+CREATE TABLE teams (
+	id SERIAL PRIMARY KEY,
+	city TEXT NOT NULL,
+	name TEXT NOT NULL UNIQUE,
+	home_color TEXT NOT NULL,
+	away_color TEXT,
+	division_id INT
+);
