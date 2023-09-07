a0a201a16864b513c518eba7057ed0c90c612079
Chako <ryland0000@gmail.com>
2023-09-07 04:57:44 -0700

    lms_courses and lms_permissions tables

--- freesaint_lms.sql
+++ freesaint_lms.sql
@@ -1,13 +1,13 @@
 CREATE DATABASE freesaint_lms;
 
-CREATE TABLE lms_login (
+CREATE TABLE lms_logins (
 	id SERIAL PRIMARY KEY,
 	login_id INT NOT NULL UNIQUE,
 	username TEXT NOT NULL UNIQUE,
 	password TEXT NOT NULL
 );
 
-CREATE TABLE app_user (
+CREATE TABLE app_users (
 	id SERIAL PRIMARY KEY,
 	name TEXT NOT NULL UNIQUE,
 	email TEXT NOT NULL UNIQUE,
@@ -21,3 +21,38 @@ CREATE TABLE app_user (
 	age INT,
 	height INT
 );
+
+CREATE TABLE lms_courses (
+    id SERIAL PRIMARY KEY,
+    course_name TEXT NOT NULL,
+    course_id TEXT NOT NULL UNIQUE,
+    course_type TEXT NOT NULL,
+    course_stu_id TEXT NOT NULL UNIQUE,
+    course_description TEXT NOT NULL,
+    completed BOOLEAN NOT NULL,
+    quizzes TEXT NOT NULL
+);
+
+CREATE TABLE lms_permissions (
+    id SERIAL PRIMARY KEY,
+    permission_id TEXT NOT NULL UNIQUE,
+    permission_name TEXT NOT NULL,
+    permission_module TEXT NOT NULL UNIQUE
+);
+
+-- 1-to-1 relationship 
+
+ALTER TABLE lms_logins
+ADD CONSTRAINT fk_lms_logins_app_users
+FOREIGN KEY (login_id)
+REFERENCES app_users;
+
+
+-- 1-to-many relationships
+
+ALTER TABLE lms_courses
+ADD CONSTRAINT fk_lms_courses_lms_permissions
+FOREIGN KEY (course_id)
+REFERENCES lms_permissions
+
+-- many-to-many relationships
