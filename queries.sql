929b9a216618662868a54280070a8b9073777a55
Chako <ryland0000@gmail.com>
2023-09-07 01:42:04 -0700

    list of customers in different states

--- queries.sql
+++ queries.sql
@@ -254,6 +254,9 @@ ORDER BY r.region_description, t.territory_description, e.last_name, e.first_nam
 
 SELECT s.state_name, s.state_abbr, c.company_name
 FROM us_states s
+RIGHT JOIN customers c 
+ON s.state_abbr = c.region
+ORDER BY state_name;
 
 
 
