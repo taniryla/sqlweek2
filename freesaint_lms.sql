fedbeb81256743e0b027bd20379a3df65f25c87d
Chako <ryland0000@gmail.com>
2023-09-07 04:28:56 -0700

    app_user table

--- freesaint_lms.sql
+++ freesaint_lms.sql
@@ -6,3 +6,18 @@ CREATE TABLE lms_login (
 	username TEXT NOT NULL UNIQUE,
 	password TEXT NOT NULL
 );
+
+CREATE TABLE app_user (
+	id SERIAL PRIMARY KEY,
+	name TEXT NOT NULL UNIQUE,
+	email TEXT NOT NULL UNIQUE,
+	googleId TEXT NOT NULL UNIQUE,
+	GWTToken TEXT NOT NULL UNIQUE,
+	profile_active BOOLEAN,
+	calculated_cal_goal INT,
+	starting_weight INT,
+	target_weight INT,
+	avatar_photo JSON,
+	age INT,
+	height INT
+);
